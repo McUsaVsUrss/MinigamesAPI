8c2f4bbf4dca1cd094b85b98fd5d05045bbac455
MatteCarra <matte.carra@gmail.com>
2015-09-17 17:33:34 +0200

    Fix project and fixed BUGS

---
+++ API/src/com/comze_instancelabs/minigamesapi/ArenaLogger.java
@@ -0,0 +1,13 @@
+package com.comze_instancelabs.minigamesapi;
+
+import org.bukkit.Bukkit;
+
+public class ArenaLogger {
+
+    public static void debug(String msg) {
+        if (MinigamesAPI.debug) {
+            Bukkit.getConsoleSender().sendMessage("[" + System.currentTimeMillis() + " MGLIB-DBG] " + msg);
+        }
+    }
+
+}
